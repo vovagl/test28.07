92dc8487ed70bee24ee7c932ccc2f41a3c5965ca
VladimirG-1971 <vova16061971@gmail.com>
2025-08-01 14:56:44 +0300

    01.08

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,19 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-build: {
-    outDir: 'dist',
-  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://109.73.206.144:6969',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })
